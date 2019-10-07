1aae6172b3ff866eb437d2a4684de2c4bb737071
Steven Doran <steven.doran@flatironschool.com>
2019-10-07 15:27:38 -0400

    delete test code

--- 14-intro-to-sinatra-and-mvc/my_app/db/migrate/20191007191815_add_clowns_table.rb
+++
@@ -1,9 +0,0 @@
-class AddClownsTable < ActiveRecord::Migration
-  def change
-    create_table :clowns do |t|
-      t.string :name
-      t.integer :scariness
-      t.timestamps
-    end
-  end
-end
